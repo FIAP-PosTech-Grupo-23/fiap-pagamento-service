6bdbc83616eb6d99cb0618ef4c41ddb6782a14ef
Guilherme Reis Lima <contato.guilhermereislima@gmail.com>
2025-07-01 20:32:59 -0300

    Creating project.

---
+++ src/main/java/br/com/fiap/fiap_pagamento_service/FiapPagamentoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiap_pagamento_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapPagamentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapPagamentoServiceApplication.class, args);
+	}
+
+}
